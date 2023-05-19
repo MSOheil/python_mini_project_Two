b6aa3f664dcdae6641f166b8876011c5ef036a42
msoheil <moh99soheil@gmail.com>
2023-05-19 06:52:37 +0330

    initial commit

--- Calculation.py
+++ Calculation.py
@@ -10,16 +10,17 @@ def calc_checket(target,num,*nums,operators='+'):
                 result-=item
     else:
         print("Please chose minus or plus ")
+    checker(target,result)
     return result
 
 
 def checker(target,result):
      if(target<result):
-          print("true target")
+          print("true target is greater than your selected target")
      elif target>result:
-          pass
+          print("true target is lower than your selected target")
      else:
           print("target and result is equal")
 
 fina= calc_checket(1,2,3,4)
-print(fina , ' this sfile ')
+print("the final resutl %d is filnal "% fina)
